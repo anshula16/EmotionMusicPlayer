52d29cf40f092163940cd3b9ba7d471774b008c9
Anisha <kathuria.anisha@gmail.com>
2019-11-07 20:51:55 +0530

    NavBar 2

--- app/src/main/java/com/example/emotionmusicplayer/MainActivity.java
+++ app/src/main/java/com/example/emotionmusicplayer/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.emotionmusicplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  implements View.OnClickListener{
 
+    Button b1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        b1 = (Button) findViewById(R.id.button1);
+        b1.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
     }
 }
